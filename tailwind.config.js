e511b636ece6f12c2b9dd570814fa151b6bdc02b
HaiBang1010 <haibang1010999@gmail.com>
2023-05-10 22:11:54 +0700

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: ({ colors }) => ({
+                blueBtn: "#2f5acf",
+                blueText: "#0939b2",
+                yellowText: "#f9f86c",
+                grayText: "#d9d9d9",
+            }),
+            maxWidth: ({ theme, breakpoints }) => ({
+                s: "10rem",
+                x: "16.25rem",
+            }),
+            screens: {
+                ml: "960px",
+            },
+        },
+    },
+    plugins: [],
+};
