5921c099d2f53ed351af401fa844ccab3e86b6ef
HaiDang1501 <haidangtr1501@gmail.com>
2023-05-14 20:53:30 +0700

    add `AdminLogin` `AdminLayout` pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState, Fragment } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Footer from "./componenets/cores/layouts/footer/Footer";
-import "./App.css";
+import Loader from "./componenets/cores/Loader";
+// import AdminLogin from "./pages/admin/Login/AdminLogin";
+import AdminLayout from "./componenets/cores/layouts/AdminLayout";
+// import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <Footer />;
+    return <AdminLayout />;
 }
 
 export default App;
---
+++ src/componenets/cores/layouts/AdminLayout.tsx
@@ -0,0 +1,180 @@
+import React, { useState, useEffect } from "react";
+import { Layout, Menu, message } from "antd";
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    CodeSandboxOutlined,
+    BookOutlined,
+    FormOutlined,
+    ContactsOutlined,
+    CommentOutlined,
+    PayCircleOutlined,
+} from "@ant-design/icons";
+import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
+import { AdminLogin } from "../../../pages/admin/Login";
+import "../../../sass/Admin/AdminLayout.scss";
+
+const AdminLayout = () => {
+    // const location = useLocation();
+    // const navigate = useNavigate();
+    // const { isAdmin } = useAuthContext();
+    const [collapsed, setCollapsed] = useState<boolean>(false);
+    const onToggle = () => {
+        setCollapsed(!collapsed);
+    };
+    const { Header, Sider, Content, Footer } = Layout;
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (!(window.innerWidth > 800) && collapsed === false) {
+                setCollapsed(true);
+            } else {
+                setCollapsed(false);
+            }
+            if (!(window.innerHeight > 400)) {
+                setCollapsed(true);
+            } else {
+                setCollapsed(false);
+            }
+        };
+        window.addEventListener("resize", handleResize, true);
+
+        return () => {
+            window.removeEventListener("resize", handleResize, true);
+        };
+    });
+
+    const items = [
+        {
+            key: "1",
+            icon: <UserOutlined />,
+            label: "User",
+            path: "users",
+        },
+        {
+            key: "2",
+            icon: <CodeSandboxOutlined />,
+            label: "Product",
+            path: "products",
+        },
+        {
+            key: "3",
+            icon: <BookOutlined />,
+            label: "Blog",
+            path: "blogs",
+        },
+        {
+            key: "4",
+            icon: <FormOutlined />,
+            label: "Bookings",
+            path: "bookings",
+        },
+        {
+            key: "5",
+            icon: <ContactsOutlined />,
+            label: "Contacts",
+            path: "contacts",
+        },
+        {
+            key: "6",
+            icon: <CommentOutlined />,
+            label: "Comments",
+            path: "comments",
+        },
+        {
+            key: "7",
+            icon: <PayCircleOutlined />,
+            label: "Bills",
+            path: "bill",
+        },
+    ];
+
+    return (
+        <Layout className="Admin_Layout" style={{ height: "100%" }}>
+            <Sider
+                width="15%"
+                style={{ height: "100vh", position: "sticky", top: "0" }}
+                theme="light"
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+            >
+                <div
+                    className="logo"
+                    style={{
+                        padding: "10px",
+                        height: "30%",
+                        position: "relative",
+                        top: "1%",
+                    }}
+                >
+                    <img
+                        src="https://senshop.tech/static/media/logo.bc588d992055212e8997a878ac242940.svg"
+                        alt="logo"
+                        style={{ width: "100%" }}
+                    />
+                </div>
+                <Menu mode="inline" defaultSelectedKeys={["1"]}>
+                    {items.map((item) => {
+                        return (
+                            <Menu.Item key={item.key} icon={item.icon}>
+                                {item.label}
+                                {/* <Link to={item.path} /> */}
+                            </Menu.Item>
+                        );
+                    })}
+                </Menu>
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                        backgroundColor: "white",
+                        position: "sticky",
+                        top: "0",
+                        zIndex: "500",
+                    }}
+                >
+                    {React.createElement(
+                        collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                        {
+                            style: { marginRight: "1%" },
+                            className: "trigger",
+                            onClick: onToggle,
+                        }
+                    )}
+                    Welcome to Admin!
+                    <AdminLogin />
+                </Header>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: "24px 16px",
+                        padding: 24,
+                        minHeight: 280,
+                        backgroundColor: "white",
+                        wordWrap: "break-word",
+                        display: "block",
+                    }}
+                >
+                    <Outlet />
+                </Content>
+                <Footer
+                    style={{
+                        backgroundColor: "white",
+                        textAlign: "center",
+                        position: "sticky",
+                        bottom: "0",
+                    }}
+                >
+                    {" "}
+                    Sen Shop &copy;2022 Created by senshop.tech
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default AdminLayout;
